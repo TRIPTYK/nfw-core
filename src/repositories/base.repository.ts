9a5f08f4c2f774cdb62b83db98dd06b78208eac6
amaury.deflorenne <amaury.deflorene@std.heh.be>
2019-09-25 15:28:14 +0200

    remove compile errors

--- src/repositories/base.repository.ts
+++ src/repositories/base.repository.ts
@@ -1,12 +1,12 @@
 import {Brackets, EntityRepository, Repository, SelectQueryBuilder} from "typeorm";
 import * as SqlString from "sqlstring";
 import {Request} from "express";
-import * as Boom from "boom";
+import Boom from "@hapi/boom";
 import * as dashify from "dashify";
 import * as JSONAPISerializer from "json-api-serializer";
 import { isPlural } from "pluralize";
-import {BaseSerializer} from "../serializers/base.serializer";
-import {JsonApiRepositoryInterface} from "../interfaces/JsonApiRepository.interface";
+import {BaseSerializer} from "..";
+import {JsonApiRepositoryInterface} from "..";
 
 /**
  * Base Repository class , inherited for all current repositories
