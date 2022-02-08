5f8129d3bc30e2339200dfb7c8649285ba084e25
AmauryD <deflorenne.amaury@gmail.com>
2022-02-08 14:09:12 +0100

    use allowed methods and bump

--- src/factories/application.factory.ts
+++ src/factories/application.factory.ts
@@ -1,12 +1,13 @@
 import Router from '@koa/router';
 import type { MikroORM } from '@mikro-orm/core';
 import type { Middleware } from 'koa';
-import Koa from 'koa'
+import Koa from 'koa';
 import { container } from 'tsyringe';
-import type { MiddlewareInterface } from '../interfaces/middleware.interface.js'
+import type { MiddlewareInterface } from '../interfaces/middleware.interface.js';
 import type { ErrorHandlerInterface, GuardInterface } from '../index.js';
 import type { Class } from '../types/class.js';
 import { createRouting } from './application-routing.factory.js';
+import HttpError from 'http-errors';
 
 interface GuardOptions {
   guard: Class<GuardInterface>,
@@ -55,12 +56,21 @@ export async function createApplication (options: CreateApplicationOptions) {
       });
     } catch (e) {
       // eslint-disable-next-line no-console
-      console.error('An error has been thrown trying to import @mikro-orm, check your dependencies !');
+      console.error(
+        'An error has been thrown trying to import @mikro-orm, check your dependencies !'
+      );
     }
   }
 
   createRouting(applicationRouter, options);
-  app.use(applicationRouter.routes());
+  app.use(applicationRouter.routes())
+  app.use(
+    applicationRouter.allowedMethods({
+      throw: true,
+      notImplemented: () => new HttpError.NotImplemented(),
+      methodNotAllowed: () => new HttpError.MethodNotAllowed()
+    })
+  );
 
   return app;
 }
