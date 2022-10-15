4229cd49400f2f71bd720bc9f0372217928be92d
AmauryD <deflorenne.amaury@gmail.com>
2022-10-15 16:52:36 +0200

    remove examples

--- examples/mikro-orm/src/models/user.model.ts
+++
@@ -1,9 +0,0 @@
-import { BaseEntity, Entity, PrimaryKey } from '@mikro-orm/core';
-
-@Entity()
-export class UserModel extends BaseEntity<UserModel, 'id'> {
-    @PrimaryKey({
-
-    })
-  declare id : string;
-}
--- examples/simple-api/src/area.ts
+++
@@ -1,9 +0,0 @@
-
-import { Controller } from '@triptyk/nfw-http';
-import { UsersController } from './controller.js';
-
-@Controller({
-  controllers: [UsersController],
-  routeName: '/api/v1'
-})
-export class Area {}
