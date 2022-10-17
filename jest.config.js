68edb86a9ddb000f1eb28ba7682d42cff88f3dd7
AmauryD <deflorenne.amaury@gmail.com>
2022-10-17 14:08:18 +0200

    fix test open handle

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,21 @@
 export default {
-  testEnvironment: 'node',
-  projects: [{
-    displayName: 'nfw-core-mikro-orm',
-    testMatch: ['<rootDir>/packages/mikro-orm/dist/tests/**/*.test.js']
-  }, {
-    displayName: 'nfw-core',
-    testMatch: ['<rootDir>/packages/core/dist/tests/**/*.test.js']
-  }, {
-    displayName: 'nfw-jsonapi',
-    testMatch: ['<rootDir>/packages/jsonapi/dist/tests/**/*.test.js']
-  }]
+  testEnvironment: "node",
+  projects: [
+    {
+      displayName: "nfw-core-mikro-orm",
+      testMatch: ["<rootDir>/packages/mikro-orm/dist/tests/**/*.test.js"],
+    },
+    {
+      displayName: "nfw-core",
+      testMatch: ["<rootDir>/packages/core/dist/tests/**/*.test.js"],
+    },
+    {
+      displayName: "nfw-http",
+      testMatch: ["<rootDir>/packages/http/dist/tests/**/*.test.js"],
+    },
+    {
+      displayName: "nfw-jsonapi",
+      testMatch: ["<rootDir>/packages/jsonapi/dist/tests/**/*.test.js"],
+    },
+  ],
 };
--- packages/jsonapi/tests/acceptance/query.test.ts
+++ packages/jsonapi/tests/acceptance/query.test.ts
@@ -1,49 +1,48 @@
-import { container } from "@triptyk/nfw-core";
-import { JsonApiMethod, JsonApiRegistry, QueryParser } from "../../src/index.js"
+import 'reflect-metadata';
+import { JsonApiMethod, JsonApiRegistry, QueryParser } from '../../src/index.js'
+import { container } from '@triptyk/nfw-core';
+import { MikroORM } from '@mikro-orm/core';
 
- beforeEach(async () => {
-    const { initApp } = await import('./dummy-app/init.js');
-    await initApp();
-  });
+beforeEach(async () => {
+  const { initApp } = await import('./dummy-app/init.js');
+  await initApp();
+});
 
 test('parses request', async () => {
-    const parser = new QueryParser();
-    const registry = container.resolve(JsonApiRegistry);
+  const parser = new QueryParser();
+  const registry = container.resolve(JsonApiRegistry);
 
-    const resource = registry.getResourceByClassName('ArticleResource')!;
-    
+  const resource = registry.getResourceByClassName('ArticleResource')!;
 
-    parser.context = {
-        resource,
-        koaContext: undefined as any,
-        method: JsonApiMethod.GET
+  parser.context = {
+    resource,
+    koaContext: undefined as any,
+    method: JsonApiMethod.GET
+  }
+
+  const parsed = await parser.parse({
+    include: 'comments',
+    filter: {
+      $and: [
+        {
+          $or: [{ comments: { id: { $eq: 'category6' } } }]
+        }
+      ]
     }
+  });
 
-    const parsed = await parser.parse({
-      include: 'comments',
-      filter: {
-        $and: [
-          {
-            $or: [{ comments: { id: { $eq: 'category6' } } }],
-          },
-        ],
-      },
-    });
+  expect(parsed.includes.size).toStrictEqual(1);
+  expect(parsed.filters.logical).toStrictEqual('$and');
 
-    expect(parsed.includes.size).toStrictEqual(1);
-    expect(parsed.filters.logical).toStrictEqual("$and");
+  let [nested] = Array.from(parsed.filters.nested.values());
 
-    let [nested] = Array.from(parsed.filters.nested.values());
+  expect(nested.logical).toStrictEqual('$and');
 
-    expect(nested.logical).toStrictEqual("$and");
+  [nested] = Array.from(nested.nested.values());
 
-    [nested] = Array.from(nested.nested.values());
+  expect(nested.logical).toStrictEqual('$or');
+});
 
-    expect(nested.logical).toStrictEqual("$or");
-})
-
-/**
- * articles : {
- *
- * }
- */
+afterEach(async () => {
+  await container.resolve(MikroORM).close(true);
+});
