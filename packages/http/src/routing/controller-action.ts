69f69710db725578af166d35dd1687be06fa468b
AmauryD <deflorenne.amaury@gmail.com>
2022-10-29 23:13:00 +0200

    refactoring till you drop

--- packages/http/src/routing/controller-action.ts
+++ packages/http/src/routing/controller-action.ts
@@ -6,7 +6,6 @@ import type { HttpEndpointMetadataArgs } from '../interfaces/endpoint.metadata.j
 import type { ResponseHandlerInterface } from '../interfaces/response-handler.js';
 import { MetadataStorage } from '../storages/metadata-storage.js';
 import type { ParamsHandleFunction, UseParamsMetadataArgs } from '../storages/metadata/use-param.js';
-import type { UseResponseHandlerMetadataArgs } from '../storages/metadata/use-response-handler.metadata.js';
 import type { GuardInstance } from './guard-action.js';
 import { isSpecialHandle, resolveSpecialContext, callGuardWithParams, resolveGuardInstance } from './guard-action.js';
 
@@ -32,22 +31,35 @@ export function resolveParams (paramsMeta: UseParamsMetadataArgs[], contextArgs:
   return Promise.all(paramsMeta.map((paramMeta) => handleParam(paramMeta, contextArgs, controllerAction, controllerInstance, ctx)));
 }
 
-function controllerActionMiddleware (guardsInstance: GuardInstance[], controllerInstance: any, routeMetadata: HttpEndpointMetadataArgs, paramsForRouteMetadata:UseParamsMetadataArgs[], responsehandlerForRouteMetadata: UseResponseHandlerMetadataArgs | undefined, responseHandlerUseParams: ResponseHandlerInstanceMeta | undefined) {
+async function executeGuards (guardsInstance: GuardInstance[], routeMetadata: HttpEndpointMetadataArgs, controllerInstance: any, ctx: RouterContext) {
+  for (const { instance, args, paramsMeta } of guardsInstance) {
+    const resolvedGuardParams = await resolveParams(paramsMeta, args, routeMetadata.propertyName, controllerInstance, ctx);
+    await callGuardWithParams(instance, resolvedGuardParams);
+  }
+}
+
+async function executeResponseHandler (responseHandlerUseParams: ResponseHandlerInstanceMeta, routeMetadata: HttpEndpointMetadataArgs, controllerInstance: any, ctx: RouterContext, controllerActionResult: any) {
+  const resolvedHandlerParams = await resolveParams(responseHandlerUseParams!.paramsMeta, responseHandlerUseParams!.args, routeMetadata.propertyName, controllerInstance, ctx);
+  return responseHandlerUseParams!.instance.handle(controllerActionResult, ...resolvedHandlerParams);
+}
+
+async function executeControllerAction (paramsForRouteMetadata: UseParamsMetadataArgs[], routeMetadata: HttpEndpointMetadataArgs, controllerInstance: any, ctx: RouterContext, controllerMethod: Function) {
+  const resolvedParams = await Promise.all(paramsForRouteMetadata.map(async (e) => handleParam(e, [], routeMetadata.propertyName, controllerInstance, ctx)));
+
+  const controllerActionResult = await controllerMethod.call(controllerInstance, ...resolvedParams);
+  return controllerActionResult;
+}
+
+function controllerActionMiddleware (guardsInstance: GuardInstance[], controllerInstance: any, routeMetadata: HttpEndpointMetadataArgs, paramsForRouteMetadata:UseParamsMetadataArgs[], responseHandlerUseParams: ResponseHandlerInstanceMeta | undefined) {
   const controllerMethod = controllerInstance[routeMetadata.propertyName] as Function;
 
   return async (ctx: RouterContext, _next: Next) => {
-    for (const { instance, args, paramsMeta } of guardsInstance) {
-      const resolvedGuardParams = await resolveParams(paramsMeta, args, routeMetadata.propertyName, controllerInstance, ctx);
-      await callGuardWithParams(instance, resolvedGuardParams);
-    }
+    await executeGuards(guardsInstance, routeMetadata, controllerInstance, ctx);
 
-    const resolvedParams = await Promise.all(paramsForRouteMetadata.map(async (e) => handleParam(e, [], routeMetadata.propertyName, controllerInstance, ctx)));
+    const controllerActionResult = await executeControllerAction(paramsForRouteMetadata, routeMetadata, controllerInstance, ctx, controllerMethod);
 
-    const controllerActionResult = await controllerMethod.call(controllerInstance, ...resolvedParams);
-
-    if (responsehandlerForRouteMetadata) {
-      const resolvedHandlerParams = await resolveParams(responseHandlerUseParams!.paramsMeta, responseHandlerUseParams!.args, routeMetadata.propertyName, controllerInstance, ctx);
-      return responseHandlerUseParams!.instance.handle(controllerActionResult, ...resolvedHandlerParams);
+    if (responseHandlerUseParams) {
+      return executeResponseHandler(responseHandlerUseParams, routeMetadata, controllerInstance, ctx, controllerActionResult);
     }
 
     ctx.response.body = controllerActionResult;
@@ -76,5 +88,5 @@ export function handleHttpRouteControllerAction (controllerInstance: any, contro
 
   const guardsInstance = guardsForRouteMetadata.map(resolveGuardInstance);
 
-  return controllerActionMiddleware(guardsInstance, controllerInstance, routeMetadata, paramsForRouteMetadata, responsehandlerForRouteMetadata, responseHandlerUseParams);
+  return controllerActionMiddleware(guardsInstance, controllerInstance, routeMetadata, paramsForRouteMetadata, responseHandlerUseParams);
 }
