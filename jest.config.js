964478ca137bd23f82d8ac0e654ed384183261c4
AmauryD <deflorenne.amaury@gmail.com>
2022-11-26 15:22:13 +0100

    ts-jest-vscode setup

--- jest.config.js
+++ jest.config.js
@@ -1,28 +1,32 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 
+const ESMPreset = {
+  preset: "ts-jest/presets/default-esm", // or other ESM presets
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
+  },
+};
+
 function createProject(name) {
   return {
     displayName: name,
     testMatch: [`<rootDir>/packages/${name}/tests/**/*.test.ts`],
-    tsConfig: `<rootDir>/packages/${name}/tsconfig.json`,
-    preset: "ts-jest/presets/default-esm", // or other ESM presets
-    moduleNameMapper: {
-      "^(\\.{1,2}/.*)\\.js$": "$1",
-    },
-    transform: {
-      "^.+\\.tsx?$": [
-        "ts-jest",
-        {
-          useESM: true,
-        },
-      ],
-    },
+    ...ESMPreset,
   };
 }
 
 export default {
   testEnvironment: "node",
   collectCoverage: Boolean(process.env.CI),
+  ...ESMPreset,
   projects: [
     createProject("mikro-orm"),
     createProject("core"),
