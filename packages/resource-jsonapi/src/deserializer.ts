f719ec0ae8be97cec078dfaaa2fa4a7a606d470c
DramixDW <verliefden.romain@gmail.com>
2023-04-20 15:08:02 +0200

    Wip: deserializer generator

--- packages/resource-jsonapi/src/deserializer.ts
+++ packages/resource-jsonapi/src/deserializer.ts
@@ -1,11 +1,16 @@
+import JSONAPISerializer from 'json-api-serializer';
 import { ResourceSchema } from './interfaces/schema';
 import { ResourcesRegistry } from './registry/registry';
 
 export class JsonApiResourceDeserializer<T extends Record<string, unknown>> {
+  private deserializer = new JSONAPISerializer();
+ 
   public constructor (
     public type: string,
     public registry: ResourcesRegistry
-  ) {}
+  ) {
+    this.generateDeserializer();
+  }
 
   get schema () {
     return this.registry.getSchemaFor(this.type) as ResourceSchema<Record<string, unknown>>;
@@ -14,5 +19,9 @@ export class JsonApiResourceDeserializer<T extends Record<string, unknown>> {
   public async deserialize (payload: Record<string, unknown>): Promise<Partial<T>> {
     const serializer = this.registry.getDeserializerFor(this.type);
     return serializer.deserialize(this.type, payload);
+  }
+
+  private generateDeserializer() {
+
   }
 }
--- packages/resource-jsonapi/src/utils/serializer-generator.ts
+++ packages/resource-jsonapi/src/utils/serializer-generator.ts
@@ -1,31 +1,24 @@
 import type JSONAPISerializer from 'json-api-serializer';
-import type {  SchemaRelationship } from '../interfaces/schema.js';
 import { ResourceSchema } from '../interfaces/schema.js';
 import type { ResourcesRegistry } from '../registry/registry';
-import {filterForWhitelist} from './whitelist-filter';
+import { filterForWhitelist } from './whitelist-filter';
 
-export class SerializerGenerator {
-  private processedTypes: string[] = [];
+abstract class AbstractSerializerGenerator {
+  protected processedTypes: string[] = [];
 
   public constructor (
-    private registry: ResourcesRegistry,
-    private serializer: JSONAPISerializer
+    protected registry: ResourcesRegistry,
+    protected serializer: JSONAPISerializer
   ) {}
 
-  public generate<T extends ResourceSchema<Record<string, unknown>>> (schema: T): void {
-    const jsonApiSchema: JSONAPISerializer.Options = {
-      relationships: {},
-      whitelist: filterForWhitelist(schema.attributes, "serialize"),
-      whitelistOnDeserialize: filterForWhitelist(schema.attributes, "deserialize"),
-    };
+  abstract generate<T extends ResourceSchema<Record<string, unknown>>>(schema: T): void 
 
+  protected generateRelationships<T extends ResourceSchema<Record<string, unknown>>>(jsonApiSchema: JSONAPISerializer.Options, schema: T) {
     for (const [relationName, relationObject] of Object.entries(schema.relationships)) {
-      if (relationObject) {
-        this.generateForRelation(jsonApiSchema, relationName, (relationObject as SchemaRelationship).type);
+      if (relationObject !== undefined) {
+        this.generateForRelation(jsonApiSchema, relationName, relationObject.type);
       }
     }
-
-    this.serializer.register(schema.type, jsonApiSchema);
   }
 
   private generateForRelation (schema: JSONAPISerializer.Options, relationName: string, relationType: string) {
@@ -38,7 +31,46 @@ export class SerializerGenerator {
     if (this.processedTypes.includes(relationSchema.type)) {
       return;
     }
+
     this.processedTypes.push(relationSchema.type);
     this.generate(relationSchema);
+  }
+}
+
+export class SerializerGenerator extends AbstractSerializerGenerator {
+  public constructor (
+    protected registry: ResourcesRegistry,
+    protected serializer: JSONAPISerializer
+  ) {
+    super(registry, serializer);
+  }
+
+  public generate<T extends ResourceSchema<Record<string, unknown>>>(schema: T): void {
+    const jsonApiSchema: JSONAPISerializer.Options = {
+      relationships: {},
+      whitelist: filterForWhitelist(schema.attributes, "serialize"),
+    };
+
+    this.generateRelationships(jsonApiSchema, schema);
+    this.serializer.register(schema.type, jsonApiSchema);
+  }
+}
+
+export class DeserializerGenerator extends AbstractSerializerGenerator {
+  public constructor (
+    protected registry: ResourcesRegistry,
+    protected serializer: JSONAPISerializer
+  ) {
+    super(registry, serializer);
+  }
+
+  public generate<T extends ResourceSchema<Record<string, unknown>>>(schema: T): void {
+    const jsonApiSchema: JSONAPISerializer.Options = {
+      relationships: {},
+      whitelistOnDeserialize: filterForWhitelist(schema.attributes, "deserialize"),
+    };
+
+    this.generateRelationships(jsonApiSchema, schema);
+    this.serializer.register(schema.type, jsonApiSchema);
   }
 }
