668f752f2a669bdfcc303c86c57f049ec8080463
AmauryD <deflorenne.amaury@gmail.com>
2024-08-14 10:00:36 +0200

    make deserializer and serializer optional and replace by a default one if not provided

--- packages/resource-jsonapi/src/registry/registry.ts
+++ packages/resource-jsonapi/src/registry/registry.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-classes-per-file */
 /* eslint-disable class-methods-use-this */
 import { container, singleton } from '@triptyk/nfw-core';
 import type { Class } from 'type-fest';
@@ -5,6 +6,8 @@ import type { ResourceDeserializer } from '../interfaces/deserializer.js';
 import type { Resource } from '../interfaces/resource.js';
 import type { ResourceSchema } from '../interfaces/schema.js';
 import type { ResourceSerializer } from '../interfaces/serializer.js';
+import { JsonApiResourceSerializer } from '../serialization/serializer.js';
+import { JsonApiResourceDeserializer } from '../serialization/deserializer.js';
 
 export interface BaseConfig {
   host: string,
@@ -35,11 +38,30 @@ export class ResourcesRegistryImpl implements ResourcesRegistry {
     return container.resolve(`deserializer:${type}`);
   }
 
+  // eslint-disable-next-line max-statements
   register<T extends Resource> (type: string, classes: {
-    serializer: Class<ResourceSerializer>,
-    deserializer: Class<ResourceDeserializer<T>>,
+    serializer?: Class<ResourceSerializer>,
+    deserializer?: Class<ResourceDeserializer<T>>,
     schema: ResourceSchema<T>,
   }): void {
+    const registry = this;
+
+    if (!classes.serializer) {
+      classes.serializer = class DefaultSerializer extends JsonApiResourceSerializer {
+        public constructor () {
+          super(registry);
+        }
+      };
+    }
+
+    if (!classes.deserializer) {
+      classes.deserializer = class DefaultDeserializer extends JsonApiResourceDeserializer<T> {
+        public constructor () {
+          super(classes.schema.resourceType, registry);
+        }
+      };
+    }
+
     container.register(`serializer:${type}`, { useClass: classes.serializer });
     container.register(`deserializer:${type}`, { useClass: classes.deserializer });
     container.register(`schema:${type}`, {
