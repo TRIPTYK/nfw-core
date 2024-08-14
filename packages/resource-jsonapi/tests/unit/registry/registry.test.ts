4664144ba31daf17abdb3598a6b7291861c67c99
AmauryD <deflorenne.amaury@gmail.com>
2024-08-14 10:49:00 +0200

    improve tests

--- packages/resource-jsonapi/tests/unit/registry/registry.test.ts
+++ packages/resource-jsonapi/tests/unit/registry/registry.test.ts
@@ -4,28 +4,87 @@ import 'reflect-metadata';
 import { container, singleton } from '@triptyk/nfw-core';
 import { beforeEach, describe, expect, it } from 'vitest';
 import { ResourcesRegistryImpl } from '../../../src/registry/registry.js';
+import { JsonApiResourceDeserializer, JsonApiResourceSerializer } from '../../../src/index.js';
 
 describe('ResourcesRegistryImpl', () => {
   let resourcesRegistry: ResourcesRegistryImpl;
 
   @singleton()
-  class ExampleSerializer {}
+  class ExampleSerializer extends JsonApiResourceSerializer {
+    public constructor () {
+      super(container.resolve(ResourcesRegistryImpl));
+    }
+  }
+
   @singleton()
-  class ExampleDeserializer {}
+  class ExampleDeserializer extends JsonApiResourceDeserializer<never> {
+    public constructor () {
+      super('example', container.resolve(ResourcesRegistryImpl));
+    }
+  }
 
   beforeEach(() => {
     resourcesRegistry = container.resolve(ResourcesRegistryImpl);
     resourcesRegistry.register('example', {
-      serializer: ExampleSerializer as never,
-      deserializer: ExampleDeserializer as never,
-      schema: {} as never,
+      serializer: ExampleSerializer,
+      deserializer: ExampleDeserializer,
+      schema: {
+        resourceType: 'example',
+        relationships: {},
+        attributes: {},
+      },
+    });
+  });
+
+  describe('register', () => {
+    it('should register a resource with its serializer, deserializer and schema', () => {
+      resourcesRegistry.register('example', {
+        serializer: ExampleSerializer,
+        deserializer: ExampleDeserializer,
+        schema: {
+          resourceType: 'example',
+          relationships: {},
+          attributes: {},
+        },
+      });
+
+      const schema = resourcesRegistry.getSchemaFor('example');
+
+      expect(schema).toMatchInlineSnapshot(`
+        {
+          "attributes": {},
+          "relationships": {},
+          "resourceType": "example",
+        }
+      `);
+    });
+    it('should register default serializer and deserializer if not provided', () => {
+      resourcesRegistry.register('example', {
+        schema: {
+          resourceType: 'example',
+          relationships: {},
+          attributes: {},
+        },
+      });
+
+      const serializer = resourcesRegistry.getSerializerFor('example');
+      const deserializer = resourcesRegistry.getDeserializerFor('example');
+
+      expect(serializer).toBeDefined();
+      expect(deserializer).toBeDefined();
     });
   });
 
   describe('getSchemaFor', () => {
     it('should resolve the schema for the given type', () => {
       const schema = resourcesRegistry.getSchemaFor('example');
-      expect(schema).toStrictEqual({});
+      expect(schema).toMatchInlineSnapshot(`
+        {
+          "attributes": {},
+          "relationships": {},
+          "resourceType": "example",
+        }
+      `);
     });
   });
 
